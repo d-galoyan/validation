908f3946ee571f55af2dea05f148deaaebee97f5
David Galoyan <david.galoyan@webbfontaine.com>
2020-06-17 15:17:34 +0400

    minor fix

--- src/utils.ts
+++ src/utils.ts
@@ -7,13 +7,3 @@ export const string = {
             || str === 'undefined'
     }
 }
-
-const test = ["error", {
-    name     : 'asd',
-    lastname : "das"
-}]
-
-const asd = "asdasdasd"
-const asdasdasdasdasd = "asdasdasd"
-
-console.log(test, asd, asdasdasdasdasd)
