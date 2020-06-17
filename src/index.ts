ca81bcef4e2649570c98c39898a8596e69dfa1d6
David Galoyan <david.galoyan@webbfontaine.com>
2020-06-17 12:07:47 +0400

    removing unnessary comment in index.js

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,2 @@
-// import Validation from "./Validation"
-
 export {default as Validation} from './Validation'
 
-/*const validation = new Validation()
-
-validation.rules({
-    name   : 'required|string|min:6|match:lastname',
-    age    : 'int|length:12-24',
-    salary : 'bail|int|min:50000|max:100000|local'
-})
-
-validation.addOnResultListener((results) => {
-    console.log(results)
-})
-
-validation.validate({
-    name   : "David",
-    age    : 28,
-    salary : 2000
-})*/
